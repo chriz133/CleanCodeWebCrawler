2a9a90f558e486c057c5e524e2b3a0552226f61d
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-04-28 17:09:32 +0200

    added test for printing

--- src/test/at/aau/crawler/utilities/CrawlerTest.java
+++ src/test/at/aau/crawler/utilities/CrawlerTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class CrawlerTest {
@@ -60,5 +64,25 @@ public class CrawlerTest {
         List<Website> result = crawler.crawlWebsite("https://example.com/", -1, List.of());
         assertNull(result);
     }
+
+    @Test
+    void testPrintWebsitesToFileValidPath() throws IOException {
+        List<Website> websites = List.of(
+                new Website(1, List.of(), List.of(), "https://example.com"),
+                new Website(2, List.of(), List.of(), "https://testWebseite.com")
+        );
+        String testDirectory = "src/main/at/aau/crawler/testFiles";
+        boolean result = crawler.printWebsitesToFile(websites, "websites.md", testDirectory);
+        assertTrue(result);
+        Path directoryPath = Paths.get(testDirectory);
+        Path filePath = directoryPath.resolve("websites.md");
+
+        assertTrue(Files.exists(filePath));
+        Files.delete(filePath);
+        Files.delete(directoryPath);
+        assertFalse(Files.exists(filePath));
+
+    }
+
 
 }

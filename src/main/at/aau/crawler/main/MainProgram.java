5a7077f426a31795e65e644e86eabe262fb1a7a8
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-04-28 13:54:06 +0200

    added command line input

--- src/main/at/aau/crawler/main/MainProgram.java
+++ src/main/at/aau/crawler/main/MainProgram.java
@@ -10,11 +10,28 @@ import java.util.stream.Collectors;
 
 public class MainProgram {
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Please enter: java MainProgram <URL> <depth> <domain1,domain2,...>");
+            System.out.println("Hint: depth must me an integer!");
+            return;
+        }
+        String url = args[0];
+        List<String> domains = Arrays.stream(args[2].split(",")).map(String::trim).collect(Collectors.toList());
+        int depth;
+        try {
+            depth = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid, depth must me an integer!");
+            return;
+        }
+
         long startTime = System.currentTimeMillis();
-        List<String> domains = Arrays.stream(new String[]{"wetter.orf.at", "sport.orf.at"}).collect(Collectors.toList());
+        //List<String> domains = Arrays.stream(new String[]{"wetter.orf.at", "sport.orf.at"}).collect(Collectors.toList());
 
         CrawlerImpl crawler = new CrawlerImpl();
-        List<Website> websites = crawler.crawlWebsite("https://www.orf.at", 5, domains);
+        //List<Website> websites = crawler.crawlWebsite("https://www.orf.at", 5, domains);
+        List<Website> websites = crawler.crawlWebsite(url, depth, domains);
+
         for (Website website2 : websites) {
             System.out.println(website2);
         }

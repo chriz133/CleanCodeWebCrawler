94e67ac8b7fd6359fbe632162d54a046417f04a3
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-04-29 19:43:39 +0200

    changed location of comments

--- src/main/at/aau/crawler/utilities/CrawlerImpl.java
+++ src/main/at/aau/crawler/utilities/CrawlerImpl.java
@@ -27,16 +27,6 @@ public class CrawlerImpl implements Crawler {
         this.alreadyVisitedUrls = new ArrayList<>();
     }
 
-    /**
-     * First all headings, links and the maximum depth of th given url is
-     * stored as a Website. This will be the root website from which the
-     * crawler will visit all links of this and all subsequent websites
-     * up to maximum depth.
-     * @param url
-     * @param maxDepth
-     * @param domains
-     * @return List<Website>
-     */
     @Override
     public List<Website> crawlWebsite(String url, int maxDepth, List<String> domains) {
         if (maxDepth < 0){
@@ -54,15 +44,6 @@ public class CrawlerImpl implements Crawler {
         return websites;
     }
 
-    /**
-     * @param path gets converted to an actual Path and if it not already exists
-     * a corresponding directory is created. A file is created with all visited
-     * websites.
-     * @param websites
-     * @param filename
-     * @param path
-     * @return boolean
-     */
     @Override
     public boolean printWebsitesToFile(List<Website> websites, String filename, String path) {
         try {

e9ccb077c8bf66990b57ddccbc522b1f60332640
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-04-29 22:15:14 +0200

    update comments

--- src/main/java/at/aau/crawler/interfaces/Crawler.java
+++ src/main/java/at/aau/crawler/interfaces/Crawler.java
@@ -11,10 +11,10 @@ public interface Crawler {
      * stored as a Website. This will be the root website from which the
      * crawler will visit all links of this and all subsequent websites
      * up to maximum depth.
-     * @param url
-     * @param maxDepth
-     * @param domains
-     * @return List<Website>
+     * @param url               the starting URL to crawl
+     * @param maxDepth          the maximum recursion depth to crawl
+     * @param domains           the list of allowed domains to include in the crawl
+     * @return List<Website>    the list of websites to write
      */
     List<Website> crawlWebsite(String url, int maxDepth, List<String> domains);
 }
